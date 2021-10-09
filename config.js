90f10b111c075d0de92bf5294c27c090be965254
dhirajbendre <75013590+dhirajbendre@users.noreply.github.com>
2021-10-09 15:51:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJ6eCGveUT-nnyY54Mf1oxUcqjv_pLh4k",
+  authDomain: "spectagram-5da5f.firebaseapp.com",
+  databaseURL: "https://spectagram-5da5f-default-rtdb.firebaseio.com",
+  projectId: "spectagram-5da5f",
+  storageBucket: "spectagram-5da5f.appspot.com",
+  messagingSenderId: "938708664153",
+  appId: "1:938708664153:web:97a447f36f8e0b3466d648"
+};
